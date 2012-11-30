30d76fb757b5a588d4d22a1673156f6c7bd73e10
Wayne Rowcliffe <war1025@gmail.com>
2012-11-29 23:42:59 -0600

    Update for 3.6

--- extension.js
+++ extension.js
@@ -1,10 +1,4 @@
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const Search = imports.ui.search;
-const SearchDisplay = imports.ui.searchDisplay;
-const IconGrid = imports.ui.iconGrid;
-const GLib = imports.gi.GLib;
-const WorkspaceSwitcherPopup = imports.ui.workspaceSwitcherPopup;
+const Meta = imports.gi.Meta;
 
 function lastWSIndex() { return global.screen.n_workspaces - 1 }
 
@@ -12,52 +6,36 @@ function indexDown(activeWorkspaceIndex) {
 	if ( activeWorkspaceIndex < lastWSIndex() ) {
 		return activeWorkspaceIndex + 1;
 	}
-  return 0;
+	return 0;
 }
 
 function indexUp(activeWorkspaceIndex) {
 	if (activeWorkspaceIndex > 0) {
-    return activeWorkspaceIndex - 1;
-  }
-  return lastWSIndex();
+		return activeWorkspaceIndex - 1;
+	}
+	return lastWSIndex();
 }
 
-let chooseIndex = { Up: indexUp, Left: indexUp, Down: indexDown, Right: indexDown };
-let chooseDir = { 	Up   : WorkspaceSwitcherPopup.UP,
-					Down : WorkspaceSwitcherPopup.DOWN,
-					Left : WorkspaceSwitcherPopup.UP,
-					Right: WorkspaceSwitcherPopup.DOWN
-				};
-
-function wrapAround(dir) {
-  return function() {
-    let activeWorkspaceIndex = global.screen.get_active_workspace_index();
-    let indexToActivate = chooseIndex[dir](activeWorkspaceIndex);
-
-    if (indexToActivate != activeWorkspaceIndex)
-      global.screen.get_workspace_by_index(indexToActivate).activate(global.get_current_time());
-
-    if (!Main.overview.visible)
-      this._workspaceSwitcherPopup.display(chooseDir[dir], indexToActivate);
-  }
+function get_neighbor(direction) {
+	var index = this.index();
+	if(direction === Meta.MotionDirection.UP || direction === Meta.MotionDirection.LEFT) {
+		index = indexUp(index);
+	} else {
+		index = indexDown(index);
+	}
+	return global.screen.get_workspace_by_index(index);
 }
 
 let old = {};
 
 function init() {
-  ['Left', 'Right', 'Up', 'Down'].forEach(function(dir) {
-    old[dir] = Main.wm['actionMoveWorkspace' + dir];
-  });
+	old = Meta.Workspace.prototype.get_neighbor;
 }
 
 function enable() {
-  ['Left', 'Right', 'Up', 'Down'].forEach(function(dir) {
-    Main.wm['actionMoveWorkspace' + dir] = wrapAround(dir);
-  });
+	Meta.Workspace.prototype.get_neighbor = get_neighbor;
 }
 
 function disable() {
-  ['Left', 'Right', 'Up', 'Down'].forEach(function(dir) {
-    Main.wm['actionMoveWorkspace' + dir] = old[dir];
-  });
+	Meta.Workspace.prototype.get_neighbor = old;
 }
